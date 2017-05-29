e926b3389359eccca518156cf5f8d8765332440b
Squigg <github@squigg.co.uk>
2017-05-29 16:28:34 +0100

    wip

---
+++ app/Settings.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Settings
+ *
+ * @mixin \Eloquent
+ * @property string value
+ * @property string key
+ */
+class Settings extends Model
+{
+
+    /**
+     * @param string $key
+     * @return mixed|string
+     */
+    static function get(string $key)
+    {
+        return static::where('key', $key)->firstOrFail()->value;
+    }
+
+    /**
+     * @param string $key
+     * @param string $value
+     * @return Model
+     */
+    static function set(string $key, string $value)
+    {
+        /** @var Settings $setting */
+        $setting = static::firstOrCreate(['key' => $key], ['value' => $value]);
+        $setting->value = $value;
+        $setting->save();
+        return $setting;
+    }
+
+    /**
+     * @return array
+     */
+    static function getAll()
+    {
+        $settings = Settings::all();
+        $array = [];
+        foreach ($settings as $setting) {
+            array_set($array, $setting->key, $setting->value);
+        }
+        return $array;
+    }
+
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Settings;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Settings::set('phone.ip','192.168.1.121');
+        Settings::set('phone.port','1817');
+        Settings::set('notification.enabled',1);
+        Settings::set('notification.pause',1);
+        Settings::set('notification.pause_duration',1);
     }
 }
