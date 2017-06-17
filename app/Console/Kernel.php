b0696e8a4382d38a377366bcff0adc8bb900a8f0
Squigg <github@squigg.co.uk>
2017-06-17 12:21:14 +0100

    Added scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,31 +1,40 @@
 <?php
 
-namespace App\Console;
 
+use App\Settings;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
     /**
      * The Artisan commands provided by your application.
      *
      * @var array
      */
-    protected $commands = [
-        //
+    protected $commands = [//
     ];
 
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            if (Settings::get('notification.paused')) {
+                /** @var Carbon $unpause */
+                $unpause = Settings::get('notification.unpause_at');
+                if (Carbon::now()->greaterThan($unpause)) {
+                    Settings::set('notification.paused', false);
+                    Settings::set('notification.enabled', true);
+                }
+            }
+        })->everyMinute();
     }
 
     /**
